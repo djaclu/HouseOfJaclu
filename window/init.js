d83ed564816d3dcfc4baceb28ab619c24098924c
djaclu <86795987+djaclu@users.noreply.github.com>
2025-10-09 19:57:22 -0400

    djacobs/init.js-runs-menuBar

--- window/init.js
+++ window/init.js
@@ -1,10 +1,13 @@
 /**
  * JACLU Website Initialization Script
- * Include this script in each page to automatically generate the footer
+ * Include this script in each page to automatically generate the footer and menu bar
  */
 
 // Import and run footer generation
 // import "./footer.js";
+
+// Import and run menu bar generation
+import "./menuBar.js";
 
 // You can add other initialization code here as needed
 console.log("JACLU website initialized");
